302c491ec9f037c0e7c4c37ab83b639883cbdc23
Sean Estabrooks <sean@eztux.com>
2025-10-16 15:01:35 -0400

    Pass desired input to select on monitor
    Turns out my monitor will only accept switch-input commands
    over the Displayport connection, so the HDMI connected computers
    can't claim the screen for themselves.
    
    So one central machine (connected to the Displayport) will be
    used to select the current input, rather than having each machine
    claim the monitor for themselves

--- monclaim.py
+++ monclaim.py
@@ -6,28 +6,36 @@ import configparser
 import shlex
 import subprocess
 import time
+from types import SimpleNamespace
 
 class ClaimMonitor:
     """
 <node>
     <interface name='com.eztux.ClaimMonitor'>
-        <method name='Switch'/>
+        <method name='Switch'>
+            <arg type='s' name='input' direction='in'/>
+        </method>
     </interface>
 </node>
     """
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('/etc/ClaimMonitor.ini')
-        switch = config.get('monitor', 'switch')
-        self.switch = shlex.split(switch)
-        probe = config.get('monitor', 'probe')
-        self.probe = shlex.split(probe)
-        self.match = config.get('monitor', 'match').strip()
-        self.error = config.get('monitor', 'error').strip()
-        self.debug = config.getboolean('monitor', 'debug')
+
+        self.last_call_time_ns = None
         lockout_seconds = config.getint('monitor', 'lockout_seconds', fallback=2)
         self.lockout_ns = lockout_seconds * 1_000_000_000  # Convert to nanoseconds
-        self.last_call_time_ns = None
+        self.debug = config.getboolean('monitor', 'debug')
+
+        self.inputs = {}
+        for input in config.sections():
+            if input != "monitor":
+                data = SimpleNamespace()
+                data.switch = shlex.split(config.get(input, 'switch'))
+                data.probe = shlex.split(config.get(input, 'probe'))
+                data.match = config.get(input, 'match').strip()
+                data.error = config.get(input, 'error').strip()
+                self.inputs[input] = data
 
     def should_process(self):
         current_time_ns = time.monotonic_ns()
@@ -45,23 +53,25 @@ class ClaimMonitor:
             current_sec = time.monotonic_ns() / 1000000000
             print(f"{current_sec}: {out}", flush=True)
 
-    def Switch(self):
+    def Switch(self, input):
         if not self.should_process():
+            self.Msg(f"Ignoring invocation [{input}]")
             return
-        self.Msg(f"--Switch invoked--")
-        run = subprocess.run(self.probe, capture_output=True, text=True)
+        self.Msg(f"--Switch invoked [{input}]--")
+        if input not in self.inputs:
+            self.Msg(f"No such input specified in config!")
+            return
+        data = self.inputs[input]
+        run = subprocess.run(data.probe, capture_output=True, text=True)
         current = run.stdout.strip()
-        if current == self.error:
+        if current == data.error:
             self.Msg(f"Monitor returned [{current}] error.")
-        elif current != self.match:
+        elif current != data.match:
             self.Msg(f"Monitor returned [{current}] which doesn't match.")
-            subprocess.run(self.switch)
+            subprocess.run(data.switch)
         else:
             self.Msg(f"Monitor is already on current input.")
 
 bus = SystemBus()
 bus.publish("com.eztux.ClaimMonitor", ClaimMonitor())
 GLib.MainLoop().run()
-
-
-
